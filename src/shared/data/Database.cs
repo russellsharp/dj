c1a41c24bc30693f74fb6c9514cf5d737c543e73
russellicious <russellsharp80@gmail.com>
2026-07-21 13:22:45 -0500

    Debugging github configuration.

--- src/shared/data/Database.cs
+++ src/shared/data/Database.cs
@@ -97,6 +97,13 @@ public class Database : IDisposable, IDatabase
             if (!System.IO.File.Exists(DatabasePath))
             {
                 Console.WriteLine("Database file does not exist and will be created.");
+
+
+
+                var processPath = Environment.ProcessPath ?? throw new InvalidOperationException("Environment.ProcessPath is null");
+                var rootDir = Path.GetDirectoryName(processPath) ?? throw new InvalidOperationException("Unable to determine process directory");
+
+                Directory.EnumerateFiles(rootDir).ToList().ForEach(Console.WriteLine);
             }
 
             var lockObject = s_databaseLocks.GetOrAdd(DatabasePath, _ => new object());
